42497e55f25d57cec293412db61e9b7f1b66a63c
Davias97 <da.vesely@email.cz>
2024-02-05 15:50:01 +0100

    finished generics section

---
+++ StudentChallenge/src/main/java/dev/lpa/model/Student.java
@@ -0,0 +1,53 @@
+package dev.lpa.model;
+
+import dev.lpa.util.QueryItem;
+
+import java.util.Comparator;
+import java.util.Random;
+
+public class Student implements QueryItem, Comparable<Student> {
+
+    private static long LAST_ID = 1000;
+    private long studentId;
+    private String name;
+    private String course;
+    private int yearStarted;
+
+    protected static Random random = new Random();
+
+    private static String[] firstNames = {"Ann", "Bill", "Cathy", "John", "Tim"};
+    private static String[] courses = {"C++", "Java", "Python"};
+
+    public Student() {
+        int lastNameIndex = random.nextInt(65, 91);
+        name = firstNames[random.nextInt(5)] + " " + (char) lastNameIndex;
+        course = courses[random.nextInt(3)];
+        yearStarted = random.nextInt(2018, 2023);
+        studentId = LAST_ID++;
+    }
+
+    @Override
+    public String toString() {
+        return "%d %-15s %-15s %d".formatted(studentId, name, course, yearStarted);
+    }
+
+    public int getYearStarted() {
+        return yearStarted;
+    }
+
+    @Override
+    public boolean matchFieldValue(String fieldName, String value) {
+        String fname = fieldName.toUpperCase();
+        return switch(fname) {
+            case "NAME" -> name.equalsIgnoreCase(value);
+            case "COURSE" -> course.equalsIgnoreCase(value);
+            case "YEARSTARTED" -> yearStarted == (Integer.parseInt(value));
+            default -> false;
+        };
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return Long.valueOf(studentId).compareTo(o.studentId);
+    }
+}
